c60e79ee0d49a25dd6090f1ef02de19fc927ae6a
Per Eckerdal <per.eckerdal@gmail.com>
2016-01-11 21:51:32 +0100

    Use Rapidcheck for tests

--- test/main.cpp
+++ test/main.cpp
@@ -1,6 +1,17 @@
 #define CATCH_CONFIG_MAIN
-#include <catch.hpp>
 
-TEST_CASE("Basic", "[basic]") {
-  REQUIRE(1 == 1);
+#include <vector>
+#include <algorithm>
+
+#include <catch.hpp>
+#include <rapidcheck/catch.h>
+
+TEST_CASE("Basic") {
+  rc::prop("double reversal yields the original value", [](
+      const std::vector<int> &l0) {
+    auto l1 = l0;
+    std::reverse(begin(l1), end(l1));
+    std::reverse(begin(l1), end(l1));
+    RC_ASSERT(l0 == l1);
+  });
 }
