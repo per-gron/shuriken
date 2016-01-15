d911b93d576a6c849d86a14ca851965b901dee1b
Per Eckerdal <per.eckerdal@gmail.com>
2016-01-15 08:54:02 +0100

    Write CMake configuration for re2c

---
+++ vendor/re2c_cmake/config.h
@@ -0,0 +1,30 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+/* Manually modified by Per Eckerdal for the CMake build of re2c */
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Name of package */
+#define PACKAGE "re2c"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "re2c-general@lists.sourceforge.net"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "re2c"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "re2c 0.15.3"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "re2c"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.15.3"
+
+/* Version number of package */
+#define VERSION "0.15.3"
