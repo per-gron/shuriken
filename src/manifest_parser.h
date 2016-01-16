e13d2ae7b64fa1480909f1c8149b9b8c44f6bd0b
Per Eckerdal <per.eckerdal@gmail.com>
2016-01-16 12:22:52 +0100

    Update todo a bit

--- src/manifest_parser.h
+++ src/manifest_parser.h
@@ -4,6 +4,7 @@
 #include <vector>
 
 #include "file_system.h"
+#include "parse_error.h"
 #include "step.h"
 
 namespace shk {
@@ -13,6 +14,6 @@ namespace shk {
  */
 std::vector<Steps> parseManifest(
     FileSystem::Stream &file_stream,
-    const std::string &path);
+    const std::string &path) throw(IoError, ParseError);
 
 }  // namespace shk
