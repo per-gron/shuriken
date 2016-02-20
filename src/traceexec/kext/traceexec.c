37e67c9463913d049244484e915f7c25212fc1bb
Per Eckerdal <per.eckerdal@gmail.com>
2016-02-20 20:31:36 +0100

    kext: Register character device at /dev/traceexec

--- src/traceexec/kext/traceexec.c
+++ src/traceexec/kext/traceexec.c
@@ -1,6 +1,7 @@
 #include <kern/debug.h>
 #include <libkern/libkern.h>
 #include <mach/mach_types.h>
+#include <miscfs/devfs/devfs.h>
 #include <sys/conf.h>
 #include <sys/proc.h>
 
@@ -40,27 +41,44 @@ static struct cdevsw traceexec_cdevsw =
 };
 
 static int traceexec_majdevno = 0;
+static void *traceexec_devfs = NULL;
 
 /* let the kernel pick the device number */
 static const int kTraceexecMajor = -24;
 
 kern_return_t traceexec_start(kmod_info_t * ki, void *d) {
-  if (0 >= traceexec_majdevno) {
-    traceexec_majdevno = cdevsw_add(kTraceexecMajor, &traceexec_cdevsw);
-
-    if (traceexec_majdevno < 0) {
-      printf("traceexec_init: failed to allocate a major number!\n");
-      return KERN_FAILURE;
-    }
-  } else {
+  if (traceexec_majdevno > 0 || traceexec_devfs != NULL) {
     panic("traceexec_init: called twice!\n");
   }
 
-  printf("TRACEEXEC STARTED\n");
+  traceexec_majdevno = cdevsw_add(kTraceexecMajor, &traceexec_cdevsw);
+  if (traceexec_majdevno < 0) {
+    printf("traceexec_init: failed to allocate a major number\n");
+    return KERN_FAILURE;
+  }
+
+  traceexec_devfs = devfs_make_node(
+      makedev(traceexec_majdevno, 0),
+      DEVFS_CHAR,
+      UID_ROOT,
+      GID_WHEEL,
+      0666,
+      "traceexec",
+      0);
+  if (traceexec_devfs == NULL) {
+    printf("traceexec_init: failed to devfs_make_node\n");
+    return KERN_FAILURE;
+  }
+
   return KERN_SUCCESS;
 }
 
 kern_return_t traceexec_stop(kmod_info_t *ki, void *d) {
+  if (traceexec_devfs != NULL) {
+    devfs_remove(traceexec_devfs);
+    traceexec_devfs = NULL;
+  }
+
   if (0 < traceexec_majdevno) {
     cdevsw_remove(traceexec_majdevno, &traceexec_cdevsw);
     traceexec_majdevno = 0;
