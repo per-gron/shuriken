8d977e977f820f91253979a6773791eaf56b9b08
Per Grön <per.eckerdal@gmail.com>
2017-01-06 11:08:27 +0100

    Integration test for simple build

--- src/shk/integrationtest/test.py
+++ src/shk/integrationtest/test.py
@@ -2,21 +2,31 @@
 # -*- coding: utf8 -*-
 
 import os
+import re
+import shutil
 import subprocess
 import unittest
 
-def with_cwd(dir):
-  def wrap(function):
-    def decorator(*args, **kwargs):
-      cwd = os.getcwd()
-      os.chdir(os.path.join(os.path.dirname(__file__), dir))
+def with_testdir(function):
+  dir = re.sub(r'^test_', '', function.__name__)
+  tempdir = os.path.join(os.path.dirname(__file__), 'tmpdir')
+  def decorator(*args, **kwargs):
+    shutil.copytree(dir, tempdir)
+    cwd = os.getcwd()
+    os.chdir(tempdir)
+    try:
       result = function(*args, **kwargs)
+    finally:
       os.chdir(cwd)
-      return result
-    return decorator
-  return wrap
+      shutil.rmtree(tempdir)
+    return result
+  return decorator
 
 shk = os.environ['SHK_PATH']
+
+def read_file(path):
+  with open(path) as f:
+    return f.read()
 
 class IntegrationTest(unittest.TestCase):
 
@@ -24,10 +34,15 @@ class IntegrationTest(unittest.TestCase):
     output = subprocess.check_output(shk + ' -h; exit 0', stderr=subprocess.STDOUT, shell=True)
     self.assertRegexpMatches(output, r'usage: shk')
 
-  @with_cwd('no_manifest')
+  @with_testdir
   def test_no_manifest(self):
     output = subprocess.check_output(shk + '; exit 0', stderr=subprocess.STDOUT, shell=True)
     self.assertRegexpMatches(output, r'error: loading \'build\.ninja\'')
+
+  @with_testdir
+  def test_simple_build(self):
+    subprocess.check_output(shk, stderr=subprocess.STDOUT, shell=True)
+    self.assertEqual(read_file('out'), 'data')
 
 if __name__ == '__main__':
     unittest.main()
