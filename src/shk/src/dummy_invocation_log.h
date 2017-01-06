7379cffcf2c992c22f948ce379d19061be4ef310
Per Grön <per.eckerdal@gmail.com>
2017-01-06 13:19:57 +0100

    Use override keyword on methods in DummyInvocationLog

--- src/shk/src/dummy_invocation_log.h
+++ src/shk/src/dummy_invocation_log.h
@@ -6,18 +6,20 @@ namespace shk {
 
 class DummyInvocationLog : public InvocationLog {
  public:
-  void createdDirectory(const std::string &path) throw(IoError) {}
+  void createdDirectory(const std::string &path)
+      throw(IoError) override {}
 
-  void removedDirectory(const std::string &path) throw(IoError) {}
+  void removedDirectory(const std::string &path)
+      throw(IoError) override {}
 
   void ranCommand(
       const Hash &build_step_hash,
       std::unordered_set<std::string> &&output_files,
       std::unordered_map<std::string, DependencyType> &&input_files)
-          throw(IoError) {}
+          throw(IoError) override {}
 
   void cleanedCommand(
-      const Hash &build_step_hash) throw(IoError) {}
+      const Hash &build_step_hash) throw(IoError) override {}
 };
 
 }  // namespace shk
