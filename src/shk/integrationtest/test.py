84ad75cfb2213dc0604bef7d7ea7847d5c050402
Per Grön <per.eckerdal@gmail.com>
2017-01-07 08:40:42 +0100

    Dedup some integration test code

--- src/shk/integrationtest/test.py
+++ src/shk/integrationtest/test.py
@@ -34,14 +34,17 @@ def write_file(path, contents):
   with open(path, 'w') as f:
     f.write(contents)
 
+def run_cmd(cmd):
+  return subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)
+
 class IntegrationTest(unittest.TestCase):
 
   def test_printusage(self):
-    output = subprocess.check_output(shk + ' -h; exit 0', stderr=subprocess.STDOUT, shell=True)
+    output = run_cmd(shk + ' -h; exit 0')
     self.assertRegexpMatches(output, r'usage: shk')
 
   def test_printtools(self):
-    output = subprocess.check_output(shk + ' -t list; exit 0', stderr=subprocess.STDOUT, shell=True)
+    output = run_cmd(shk + ' -t list; exit 0')
     self.assertRegexpMatches(output, r'shk subtools:')
     self.assertRegexpMatches(output, r'clean')
     self.assertRegexpMatches(output, r'commands')
@@ -52,36 +55,36 @@ class IntegrationTest(unittest.TestCase):
 
   @with_testdir('no_manifest')
   def test_no_manifest(self):
-    output = subprocess.check_output(shk + '; exit 0', stderr=subprocess.STDOUT, shell=True)
+    output = run_cmd(shk + '; exit 0')
     self.assertRegexpMatches(output, r'error: loading \'build\.ninja\'')
 
   @with_testdir('simple_build')
   def test_simple_build(self):
-    subprocess.check_output(shk, stderr=subprocess.STDOUT, shell=True)
+    run_cmd(shk)
     self.assertEqual(read_file('out'), 'data')
 
   @with_testdir('mkdir')
   def test_mkdir_build(self):
-    subprocess.check_output(shk, stderr=subprocess.STDOUT, shell=True)
+    run_cmd(shk)
     self.assertTrue(os.path.isdir('out'))
 
   @with_testdir('simple_build')
   def test_simple_rebuild(self):
-    subprocess.check_output(shk, stderr=subprocess.STDOUT, shell=True)
-    output = subprocess.check_output(shk, stderr=subprocess.STDOUT, shell=True)
+    run_cmd(shk)
+    output = run_cmd(shk)
     self.assertEqual(read_file('out'), 'data')
     self.assertRegexpMatches(output, 'no work to do')
 
   @with_testdir('target_in_subdir')
   def test_rebuild_with_moved_target(self):
-    subprocess.check_output(shk, stderr=subprocess.STDOUT, shell=True)
+    run_cmd(shk)
     self.assertEqual(read_file('dir/out'), 'hello')
 
     # Change the manifest
     manifest = read_file('build.ninja')
     write_file('build.ninja', manifest.replace('dir/out', 'dir2/out'))
 
-    subprocess.check_output(shk, stderr=subprocess.STDOUT, shell=True)
+    run_cmd(shk)
     # Since the target has moved, the old output should have been removed,
     # including its directory that was created by the build system.
     self.assertFalse(os.path.exists('dir'))
@@ -89,72 +92,72 @@ class IntegrationTest(unittest.TestCase):
 
   @with_testdir('simple_build')
   def test_noop_rebuild_after_touching_input(self):
-    subprocess.check_output(shk, stderr=subprocess.STDOUT, shell=True)
+    run_cmd(shk)
     time.sleep(1)  # Wait, because the fs mtime only has second resolution
     os.utime('in', None)  # Touch the input file
-    output = subprocess.check_output(shk, stderr=subprocess.STDOUT, shell=True)
+    output = run_cmd(shk)
     self.assertRegexpMatches(output, 'no work to do')
 
   @with_testdir('simple_build')
   def test_simple_noop_build(self):
-    subprocess.check_output(shk + ' -n', stderr=subprocess.STDOUT, shell=True)
+    run_cmd(shk + ' -n')
     self.assertFalse(os.path.exists('out'))
 
   @with_testdir('append_output')
   def test_delete_before_rebuilding(self):
-    subprocess.check_output(shk, stderr=subprocess.STDOUT, shell=True)
+    run_cmd(shk)
     self.assertEqual(read_file('out'), 'hello')
     write_file('in', 'changed')
-    subprocess.check_output(shk, stderr=subprocess.STDOUT, shell=True)
+    run_cmd(shk)
     self.assertEqual(read_file('out'), 'changed')
 
   @with_testdir('undeclared_dependency')
   def test_undeclared_dependency(self):
-    subprocess.check_output(shk, stderr=subprocess.STDOUT, shell=True)
+    run_cmd(shk)
     self.assertEqual(read_file('out'), 'input_file')
     write_file('in', 'changed')
-    subprocess.check_output(shk, stderr=subprocess.STDOUT, shell=True)
+    run_cmd(shk)
     self.assertEqual(read_file('out'), 'changed')
 
   @with_testdir('cyclic_dependency')
   def test_cyclic_dependency_specified_target(self):
-    output = subprocess.check_output(shk + ' out; exit 0', stderr=subprocess.STDOUT, shell=True)
+    output = run_cmd(shk + ' out; exit 0')
     self.assertRegexpMatches(output, r'dependency cycle: in -> out -> in$')
 
   @with_testdir('simple_build')
   def test_specify_manifest(self):
     os.rename('build.ninja', 'manifest.ninja')
-    subprocess.check_output(shk + ' -f manifest.ninja', stderr=subprocess.STDOUT, shell=True)
+    run_cmd(shk + ' -f manifest.ninja')
     self.assertEqual(read_file('out'), 'data')
 
   @with_testdir('target_in_subdir')
   def test_target_in_subdir(self):
-    subprocess.check_output(shk, stderr=subprocess.STDOUT, shell=True)
+    run_cmd(shk)
     self.assertEqual(read_file('dir/out'), 'hello')
 
   @with_testdir('simple_build')
   def test_full_clean(self):
-    subprocess.check_output(shk, stderr=subprocess.STDOUT, shell=True)
+    run_cmd(shk)
     self.assertTrue(os.path.exists('out'))
     self.assertTrue(os.path.exists('.shk_log'))
-    output = subprocess.check_output(shk + ' -t clean', stderr=subprocess.STDOUT, shell=True)
+    output = run_cmd(shk + ' -t clean')
     self.assertFalse(os.path.exists('out'))
     self.assertFalse(os.path.exists('.shk_log'))
     self.assertRegexpMatches(output, r'cleaned 2 files\.')
 
   @with_testdir('simple_build')
   def test_full_clean_again(self):
-    subprocess.check_output(shk, stderr=subprocess.STDOUT, shell=True)
-    subprocess.check_output(shk + ' -t clean', stderr=subprocess.STDOUT, shell=True)
-    output = subprocess.check_output(shk + ' -t clean', stderr=subprocess.STDOUT, shell=True)
+    run_cmd(shk)
+    run_cmd(shk + ' -t clean')
+    output = run_cmd(shk + ' -t clean')
     self.assertRegexpMatches(output, r'cleaned 0 files\.')
 
   @with_testdir('simple_build')
   def test_single_target_clean(self):
-    subprocess.check_output(shk, stderr=subprocess.STDOUT, shell=True)
+    run_cmd(shk)
     self.assertTrue(os.path.exists('out'))
     self.assertTrue(os.path.exists('.shk_log'))
-    output = subprocess.check_output(shk + ' -t clean out', stderr=subprocess.STDOUT, shell=True)
+    output = run_cmd(shk + ' -t clean out')
     self.assertFalse(os.path.exists('out'))
     self.assertTrue(os.path.exists('.shk_log'))
     self.assertRegexpMatches(output, r'cleaned 1 file\.')
