d87deb45cefda6675984277932daf8c4b0669da0
Per Grön <per.eckerdal@gmail.com>
2017-02-15 19:30:49 +0100

    Remove unnecessary macro

--- sysctl_helpers.h
+++ sysctl_helpers.h
@@ -28,26 +28,27 @@ void set_kdebug_numbufs(int nbufs) {
   }
 }
 
-#define ENCODE_CSC_LOW(klass, subclass) \
-  ( (uint16_t) ( ((klass) & 0xff) << 8 ) | ((subclass) & 0xff) )
+int kdebugFilterIndex(int klass, int subclass) {
+  return ((klass & 0xff) << 8) | (subclass & 0xff);
+}
 
 void set_kdebug_filter() {
   std::bitset<KDBG_TYPEFILTER_BITMAP_SIZE * 8> filter{};
 
-  filter.set(ENCODE_CSC_LOW(DBG_TRACE, DBG_TRACE_DATA));
-  filter.set(ENCODE_CSC_LOW(DBG_TRACE, DBG_TRACE_STRING));
-  filter.set(ENCODE_CSC_LOW(DBG_MACH, DBG_MACH_EXCP_SC));
-  filter.set(ENCODE_CSC_LOW(DBG_FSYSTEM, DBG_FSRW));
-  filter.set(ENCODE_CSC_LOW(DBG_FSYSTEM, DBG_BOOTCACHE));
-  filter.set(ENCODE_CSC_LOW(DBG_BSD, DBG_BSD_EXCP_SC));
-  filter.set(ENCODE_CSC_LOW(DBG_BSD, DBG_BSD_PROC));
-  filter.set(ENCODE_CSC_LOW(DBG_BSD, DBG_BSD_SC_EXTENDED_INFO));
-  filter.set(ENCODE_CSC_LOW(DBG_BSD, DBG_BSD_SC_EXTENDED_INFO2));
-  filter.set(ENCODE_CSC_LOW(FILEMGR_CLASS, 0)); // Carbon File Manager
-  filter.set(ENCODE_CSC_LOW(FILEMGR_CLASS, 1)); // Carbon File Manager
+  filter.set(kdebugFilterIndex(DBG_TRACE, DBG_TRACE_DATA));
+  filter.set(kdebugFilterIndex(DBG_TRACE, DBG_TRACE_STRING));
+  filter.set(kdebugFilterIndex(DBG_MACH, DBG_MACH_EXCP_SC));
+  filter.set(kdebugFilterIndex(DBG_FSYSTEM, DBG_FSRW));
+  filter.set(kdebugFilterIndex(DBG_FSYSTEM, DBG_BOOTCACHE));
+  filter.set(kdebugFilterIndex(DBG_BSD, DBG_BSD_EXCP_SC));
+  filter.set(kdebugFilterIndex(DBG_BSD, DBG_BSD_PROC));
+  filter.set(kdebugFilterIndex(DBG_BSD, DBG_BSD_SC_EXTENDED_INFO));
+  filter.set(kdebugFilterIndex(DBG_BSD, DBG_BSD_SC_EXTENDED_INFO2));
+  filter.set(kdebugFilterIndex(FILEMGR_CLASS, 0)); // Carbon File Manager
+  filter.set(kdebugFilterIndex(FILEMGR_CLASS, 1)); // Carbon File Manager
 
   static int name[] = { CTL_KERN, KERN_KDEBUG, KERN_KDSET_TYPEFILTER };
-  size_t len = KDBG_TYPEFILTER_BITMAP_SIZE;
+  size_t len = filter.size() / 8;
   if (sysctl(name, 3, &filter, &len, NULL, 0)) {
     throw std::runtime_error("Failed KERN_KDSET_TYPEFILTER sysctl");
   }
