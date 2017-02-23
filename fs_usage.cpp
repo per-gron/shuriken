8292d1d92d52f4457f2c991bfb2ded89d11dff8b
Per Grön <per.eckerdal@gmail.com>
2017-02-23 08:07:06 +0100

    Move event_info_map into its own file

--- fs_usage.cpp
+++ fs_usage.cpp
@@ -67,6 +67,7 @@ clang++ -std=c++11 -I/System/Library/Frameworks/System.framework/Versions/B/Priv
 #import <mach/clock_types.h>
 #import <mach/mach_time.h>
 
+#include "event_info.h"
 #include "syscall_constants.h"
 #include "syscall_tables.h"
 #include "sysctl_helpers.h"
@@ -74,105 +75,10 @@ clang++ -std=c++11 -I/System/Library/Frameworks/System.framework/Versions/B/Priv
 #define NUMPARMS 23
 #define PATHLENGTH (NUMPARMS*sizeof(uintptr_t))
 
-#define MAX_PATHNAMES 3
-#define MAX_SCALL_PATHNAMES 2
-
-struct lookup {
-  uintptr_t pathname[NUMPARMS + 1]; /* add room for null terminator */
-};
-
-struct event_info {
-  event_info()
-      : pathptr(&lookups[0].pathname[0]) {
-    for (int i = 0; i < MAX_PATHNAMES; i++) {
-      lookups[i].pathname[0] = 0;
-    }
-  }
-
-  uintptr_t child_thread = 0;
-  int pid = 0;
-  int type = 0;
-  int arg1 = 0;
-  int arg2 = 0;
-  int arg3 = 0;
-  int arg4 = 0;
-  int arg5 = 0;
-  int arg6 = 0;
-  int arg7 = 0;
-  int arg8 = 0;
-  uint64_t vnodeid = 0;
-  uintptr_t *pathptr = nullptr;
-  int pn_scall_index = 0;
-  int pn_work_index = 0;
-  struct lookup lookups[MAX_PATHNAMES];
-};
-
-
 struct threadmap_entry {
   unsigned int tm_setsize = 0; /* this is a bit count */
   unsigned long *tm_setptr = nullptr;  /* file descripter bitmap */
   char tm_command[MAXCOMLEN + 1];
-};
-
-class event_info_map {
-  struct ei_hash {
-    using argument_type = std::pair<uintptr_t, int>;
-    using result_type = std::size_t;
-
-    result_type operator()(const argument_type &f) const {
-      return
-          std::hash<uintptr_t>()(f.first) ^
-          std::hash<int>()(f.second);
-    }
-  };
-
-  using map = std::unordered_map<std::pair<uintptr_t, int>, event_info, ei_hash>;
-
- public:
-  using iterator = map::iterator;
-
-  void clear() {
-    _map.clear();
-    _last_event_map.clear();
-  }
-
-  void erase(iterator iter) {
-    auto thread = iter->first.first;
-    auto type = iter->first.second;
-
-    auto last_event_it = _last_event_map.find(thread);
-    if (last_event_it != _last_event_map.end() && last_event_it->second == type) {
-      _last_event_map.erase(last_event_it);
-    }
-
-    _map.erase(iter);
-  }
-
-  iterator add_event(uintptr_t thread, int type) {
-    _map[std::make_pair(thread, type)] = event_info();
-    _last_event_map[thread] = type;
-    return _map.find(std::make_pair(thread, type));
-  }
-
-  iterator find(uintptr_t thread, int type) {
-    return _map.find(std::make_pair(thread, type));
-  }
-
-  iterator find_last(uintptr_t thread) {
-    auto it = _last_event_map.find(thread);
-    return it == _last_event_map.end() ?
-        end() :
-        _map.find(std::make_pair(thread, it->second));
-  }
-
-  iterator end() {
-    return _map.end();
-  }
-
- private:
-  map _map;
-  // Map from thread id to last event type for that thread
-  std::unordered_map<uintptr_t, int> _last_event_map;
 };
 
 std::unordered_map<uintptr_t, threadmap_entry> threadmap;
