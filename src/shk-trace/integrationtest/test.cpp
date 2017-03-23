34bea913925e8d10dedc0480f9a616af4d8e0a50
Per Grön <per.eckerdal@gmail.com>
2017-03-23 18:52:38 +0100

    Work on integration test helper fork test
    The helper itself is done, but the test doesn't pass yet.

--- src/shk-trace/integrationtest/test.cpp
+++ src/shk-trace/integrationtest/test.cpp
@@ -1,22 +1,74 @@
+#include <cassert>
+#include <cerrno>
 #include <cstdio>
+#include <cstdlib>
+#include <fcntl.h>
 #include <string>
+#include <sys/syslimits.h>
+#include <unistd.h>
 #include <unordered_map>
 
 #include <util/file_descriptor.h>
 
 namespace {
 
-int fork() {
-  printf("fork\n");
+void die(const std::string &reason) {
+  fprintf(stderr, "Fatal error: %s (%s)\n", reason.c_str(), strerror(errno));
+  exit(1);
+}
 
-  // Fork:
-  // * inherit fd
+std::string getFdPath(int fd) {
+  char file_path[PATH_MAX];
+  if (fcntl(fd, F_GETPATH, file_path) == -1) {
+    die("Failed to get fd path");
+  }
+  return file_path;
+}
+
+shk::FileDescriptor openFileForReading(const std::string &path) {
+  int fd = open(path.c_str(), O_RDONLY);
+  if (fd == -1) {
+    die("Failed to open file for reading " + path);
+  }
+  return shk::FileDescriptor(fd);
+}
+
+shk::FileDescriptor openFileForWriting(const std::string &path) {
+  int fd = open(path.c_str(), O_WRONLY | O_CREAT | O_TRUNC | O_CLOEXEC, 0666);
+  if (fd == -1) {
+    die("Failed to open file writing " + path);
+  }
+  return shk::FileDescriptor(fd);
+}
+
+int testForkInheritFd() {
+  // Verify that file descriptors are inherited
+
+  auto usr_fd = openFileForReading("/usr");
+
+  pid_t pid = fork();
+  if (pid == -1) {
+    die("Failed to fork");
+  } else if (pid == 0) {
+    // In child
+    assert(openat(
+        usr_fd.get(), "nonexisting_path_just_for_testing", O_RDONLY) == -1);
+  } else {
+    // In parent
+    int status;
+    if (waitpid(pid, &status, 0) == -1) {
+      die("Failed to wait for child");
+    }
+    if (status != 0) {
+      die("Child failed");
+    }
+  }
 
   return 0;
 }
 
 const std::unordered_map<std::string, std::function<int ()>> kTests = {
-  { "fork", fork }
+  { "fork_inherit_fd", testForkInheritFd }
 };
 
 }  // anonymous namespace
