727b4f6d1aff6d578b1f7bbe31222aa3f57eafe6
Per Grön <per.eckerdal@gmail.com>
2017-03-31 11:24:26 +0200

    Add basic unit test for TraceWriter

---
+++ src/shk-trace/test/trace_writer_test.cpp
@@ -0,0 +1,27 @@
+#include <catch.hpp>
+
+#include "trace_writer.h"
+
+namespace shk {
+
+TEST_CASE("TraceWriter") {
+  int fd[2];
+  REQUIRE(pipe(fd) == 0);
+  FileDescriptor input_fd(fd[0]);
+  FileDescriptor output_fd(fd[1]);
+
+  {
+    TraceWriter writer(std::unique_ptr<TracingServer::TraceRequest>(
+        new TracingServer::TraceRequest(std::move(output_fd), 0, "cwd")));
+
+    writer.fileEvent(1, EventType::READ, 2, "path1");
+    writer.fileEvent(1, EventType::WRITE, 2, "path2");
+  }
+
+  char raw_buf[1024];
+  int len = read(input_fd.get(), &raw_buf, 1024);
+  std::string buf(raw_buf, len);
+  CHECK(buf == "read path1\nwrite path2\n");
+}
+
+}  // namespace shk
