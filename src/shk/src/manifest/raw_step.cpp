004259c7718a2f007a3626045f786370cbaf0053
Per Grön <per.eckerdal@gmail.com>
2017-04-09 17:53:24 +0200

    Add RawStep class
    The plan is for RawManifest to use it, and then for Step to step by step
    start deviating from RawStep.

---
+++ src/shk/src/manifest/raw_step.cpp
@@ -0,0 +1,38 @@
+#include "manifest/raw_step.h"
+
+#include <blake2.h>
+
+namespace shk {
+
+Hash RawStep::hash() const {
+  Hash hash;
+  blake2b_state state;
+  blake2b_init(&state, hash.data.size());
+  const auto hash_string = [&](const std::string &string) {
+    blake2b_update(
+        &state,
+        reinterpret_cast<const uint8_t *>(string.data()),
+        string.size() + 1);  // Include trailing \0
+  };
+
+  const auto hash_paths = [&](const std::vector<Path> &paths) {
+    for (const auto &path : paths) {
+      hash_string(path.original());
+    }
+    // Add a separator, so that it is impossible to get the same hash by just
+    // removing a path from the end of one list and adding it to the beginning
+    // of the next.
+    hash_string("");  // "" is not a valid path so it is a good separator
+  };
+  hash_paths(inputs);
+  hash_paths(implicit_inputs);
+  hash_paths(dependencies);
+  hash_paths(outputs);
+  hash_string(generator ? "" : command);
+  hash_string(rspfile_content);
+
+  blake2b_final(&state, hash.data.data(), hash.data.size());
+  return hash;
+}
+
+}  // namespace shk
---
+++ src/shk/test/manifest/raw_step_test.cpp
@@ -0,0 +1,122 @@
+#include <catch.hpp>
+
+#include "fs/path.h"
+#include "manifest/raw_step.h"
+
+#include "../in_memory_file_system.h"
+
+namespace shk {
+
+TEST_CASE("RawStep") {
+  SECTION("Hash") {
+    InMemoryFileSystem fs;
+    Paths paths(fs);
+
+    RawStep empty;
+
+    RawStep a;
+    a.inputs.push_back(paths.get("input"));
+    a.implicit_inputs.push_back(paths.get("implicit_input"));
+    a.dependencies.push_back(paths.get("dependencies_input"));
+    a.outputs.push_back(paths.get("output"));
+    a.pool_name = "pool";
+    a.command = "cmd";
+    a.description = "desc";
+    a.depfile = paths.get("dep");
+    a.rspfile = paths.get("rsp");
+    a.rspfile_content = "rsp_content";
+
+    RawStep b = a;
+
+    SECTION("Basic") {
+      CHECK(empty.hash() == empty.hash());
+      CHECK(a.hash() == a.hash());
+      CHECK(a.hash() != empty.hash());
+    }
+
+    SECTION("Bleed") {
+      a.dependencies = { paths.get("a") };
+      a.outputs = { paths.get("b") };
+      b.dependencies = {};
+      b.outputs = { paths.get("a"), paths.get("b") };
+      CHECK(a.hash() != b.hash());
+    }
+
+    SECTION("Input") {
+      b.inputs.clear();
+      CHECK(a.hash() != b.hash());
+    }
+
+    SECTION("ImplicitInputs") {
+      b.implicit_inputs.clear();
+      CHECK(a.hash() != b.hash());
+    }
+
+    SECTION("Dependencies") {
+      b.dependencies.clear();
+      CHECK(a.hash() != b.hash());
+    }
+
+    SECTION("Outputs") {
+      b.outputs.clear();
+      CHECK(a.hash() != b.hash());
+    }
+
+    SECTION("PoolName") {
+      // The pool is not significant for the build results
+      b.pool_name.clear();
+      CHECK(a.hash() == b.hash());
+    }
+
+    SECTION("Command") {
+      b.command.clear();
+      CHECK(a.hash() != b.hash());
+    }
+
+    SECTION("PoolName") {
+      // The description is not significant for the build results
+      b.description.clear();
+      CHECK(a.hash() == b.hash());
+    }
+
+    SECTION("Depfile") {
+      // The depfile path is not significant for the build results
+      b.depfile = paths.get("other");
+      CHECK(a.hash() == b.hash());
+    }
+
+    SECTION("Generator") {
+      b.generator = !b.generator;
+      CHECK(a.hash() != b.hash());
+    }
+
+    SECTION("GeneratorCommandline") {
+      // Generator rules don't include the command line when calculating
+      // dirtiness
+      a.generator = b.generator = true;
+      b.command = a.command + "somethingelse";
+      CHECK(a.hash() == b.hash());
+    }
+
+    SECTION("Rspfile") {
+      // The rspfile path is not significant for the build results
+      b.rspfile = paths.get("other");
+      CHECK(a.hash() == b.hash());
+    }
+
+    SECTION("RspfileContent") {
+      b.rspfile_content = "other";
+      CHECK(a.hash() != b.hash());
+    }
+  }
+
+  SECTION("isConsolePool") {
+    CHECK(!isConsolePool(""));
+    CHECK(!isConsolePool("a"));
+    CHECK(!isConsolePool("consol"));
+    CHECK(!isConsolePool("console_"));
+    CHECK(isConsolePool("console"));
+  }
+}
+
+}  // namespace shk
