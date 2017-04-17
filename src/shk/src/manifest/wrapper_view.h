39fbc3334fa78fd23118c7ca59f1a6086f05b541
Per Grön <per.eckerdal@gmail.com>
2017-04-17 09:50:42 +0200

    Add WrapperView

---
+++ src/shk/src/manifest/wrapper_view.h
@@ -0,0 +1,178 @@
+#pragma once
+
+#include <utility>
+#include <type_traits>
+
+namespace shk {
+
+/**
+ * WrapperView is a helper class for classes that wrap Flatbuffer objects in a
+ * nicer interface, like Step and CompiledManifest. It is used in places where
+ * they have fields that are arrays, where each item in the array should be
+ * wrapped in something else, for example a flatbuffers::String * that should be
+ * converted to a nt_string_view.
+ *
+ * This class allows exposing such lists of items in an std::vector-like
+ * interface without actually iterating over all the items at construction time.
+ * It is a view that wraps the items lazily.
+ */
+template <typename Iter, typename Wrapper>
+class WrapperView {
+ public:
+  using value_type = decltype(Wrapper(*std::declval<Iter>()));
+  using size_type = size_t;
+  using diff_type = ptrdiff_t;
+  using reference = value_type &;
+  using const_reference = const value_type &;
+  using pointer = value_type *;
+  using const_pointer = const value_type *;
+
+  class iterator {
+   public:
+    explicit iterator(Iter i) : _i(i) {}
+    iterator(const iterator &) = default;
+    iterator(iterator &&) = default;
+    iterator &operator=(const iterator &) = default;
+    iterator &operator=(iterator &&) = default;
+
+    friend void swap(iterator &a, iterator &b) {
+      std::swap(a._i, b._i);
+    }
+
+    value_type operator*() const {
+      return Wrapper(*_i);
+    }
+
+    // Lacking operator-> because we can't return a pointer to Wrapper(*_i)
+
+    iterator &operator++() {
+      ++_i;
+      return *this;
+    }
+
+    iterator operator++(int) {
+      auto copy = *this;
+      ++_i;
+      return copy;
+    }
+
+    iterator &operator--() {
+      --_i;
+      return *this;
+    }
+
+    iterator operator--(int) {
+      auto copy = *this;
+      --_i;
+      return copy;
+    }
+
+    bool operator==(iterator other) const {
+      return _i == other._i;
+    }
+
+    bool operator!=(iterator other) const {
+      return _i != other._i;
+    }
+
+    iterator operator+(diff_type diff) const {
+      return iterator(_i + diff);
+    }
+
+    iterator operator-(diff_type diff) const {
+      return iterator(_i - diff);
+    }
+
+    diff_type operator-(iterator other) const {
+      return _i - other._i;
+    }
+
+   private:
+    Iter _i;
+  };
+
+  using const_iterator = iterator;
+
+  WrapperView(Iter begin, Iter end)
+      : _begin(begin), _end(end) {}
+  WrapperView(const WrapperView &) = default;
+  WrapperView(WrapperView &&) = default;
+  WrapperView &operator=(const WrapperView &) = default;
+  WrapperView &operator=(WrapperView &&) = default;
+
+  value_type at(size_type pos) const {
+    if (!(pos < size())) {
+      throw std::out_of_range("WrapperView");
+    }
+    return Wrapper(*(_begin + pos));
+  }
+
+  value_type operator[](size_type pos) const {
+    return Wrapper(*(_begin + pos));
+  }
+
+  value_type front() const {
+    return Wrapper(*_begin);
+  }
+
+  value_type back() const {
+    return Wrapper(*(_end - 1));
+  }
+
+  const_iterator begin() const {
+    return iterator(_begin);
+  }
+
+  const_iterator cbegin() const {
+    return iterator(_begin);
+  }
+
+  const_iterator end() const {
+    return iterator(_end);
+  }
+
+  const_iterator cend() const {
+    return iterator(_end);
+  }
+
+  bool empty() const {
+    return _begin == _end;
+  }
+
+  size_type size() const {
+    return _end - _begin;
+  }
+
+  friend void swap(WrapperView &a, WrapperView &b) {
+    std::swap(a._begin, b._begin);
+    std::swap(a._end, b._end);
+  }
+
+  bool operator==(WrapperView other) const {
+    if (other._begin == _begin && other._end == _end) {
+      return true;
+    }
+
+    if (other.size() != size()) {
+      return false;
+    }
+
+    for (size_type i = 0; i < size(); i++) {
+      if ((*this)[i] != other[i]) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  bool operator!=(WrapperView other) const {
+    return !(*this == other);
+  }
+
+ private:
+  Iter _begin;
+  Iter _end;
+};
+
+}  // namespace shk
---
+++ src/shk/test/manifest/wrapper_view_test.cpp
@@ -0,0 +1,270 @@
+#include <catch.hpp>
+
+#include <vector>
+
+#include "manifest/wrapper_view.h"
+#include "string_view.h"
+
+namespace shk {
+
+TEST_CASE("WrapperView") {
+  using WV = WrapperView<std::vector<std::string>::const_iterator, string_view>;
+  auto empty = std::vector<std::string>{};
+  auto one = std::vector<std::string>{ "hi" };
+  auto two = std::vector<std::string>{ "one", "two" };
+
+  SECTION("iterator") {
+    using Iter = WV::iterator;
+
+    SECTION("Constructor") {
+      SECTION("By iterator") {
+        Iter(empty.begin());
+      }
+
+      SECTION("Copy") {
+        auto a = Iter(empty.begin());
+        auto b = a;
+        CHECK(a == b);
+      }
+
+      SECTION("Move") {
+        auto a = Iter(empty.begin());
+        auto b = std::move(a);
+        CHECK(a == b);
+      }
+    }
+
+    SECTION("Assign") {
+      SECTION("Copy") {
+        auto a = Iter(empty.begin());
+        auto b = Iter(one.begin());
+        b = a;
+        CHECK(a == b);
+      }
+
+      SECTION("Move") {
+        auto a = Iter(empty.begin());
+        auto b = Iter(one.begin());
+        b = std::move(a);
+        CHECK(b == Iter(empty.begin()));
+      }
+    }
+
+    SECTION("swap") {
+      auto a = Iter(one.begin());
+      auto b = Iter(two.begin());
+
+      std::swap(a, b);
+
+      CHECK(a == Iter(two.begin()));
+      CHECK(b == Iter(one.begin()));
+    }
+
+    SECTION("operator*") {
+      CHECK(*Iter(one.begin()) == "hi");
+    }
+
+    SECTION("prefix operator++") {
+      auto a = Iter(one.begin());
+      CHECK(++a == Iter(one.end()));
+    }
+
+    SECTION("postfix operator++") {
+      auto a = Iter(one.begin());
+      CHECK(a++ == Iter(one.begin()));
+      CHECK(a == Iter(one.end()));
+    }
+
+    SECTION("prefix operator--") {
+      auto a = Iter(one.end());
+      CHECK(--a == Iter(one.begin()));
+    }
+
+    SECTION("postfix operator--") {
+      auto a = Iter(one.end());
+      CHECK(a-- == Iter(one.end()));
+      CHECK(a == Iter(one.begin()));
+    }
+
+    SECTION("operator==") {
+      CHECK(Iter(one.begin()) == Iter(one.begin()));
+      CHECK(!(Iter(one.begin()) == Iter(one.end())));
+    }
+
+    SECTION("operator!=") {
+      CHECK(!(Iter(one.begin()) != Iter(one.begin())));
+      CHECK(Iter(one.begin()) != Iter(one.end()));
+    }
+
+    SECTION("operator+") {
+      CHECK(Iter(one.begin()) + 1 == Iter(one.end()));
+      CHECK(Iter(one.begin()) + 0 == Iter(one.begin()));
+    }
+
+    SECTION("diff_type operator-") {
+      CHECK(Iter(one.end()) - 1 == Iter(one.begin()));
+      CHECK(Iter(one.end()) - 0 == Iter(one.end()));
+    }
+
+    SECTION("iterator operator-") {
+      CHECK(Iter(one.end()) - Iter(one.begin()) == 1);
+      CHECK(Iter(one.end()) - Iter(one.end()) == 0);
+    }
+  }
+
+  SECTION("Constructor") {
+    SECTION("Range") {
+      WV(empty.begin(), empty.end());
+    }
+
+    SECTION("Copy") {
+      auto a = WV(empty.begin(), empty.end());
+      auto b = a;
+      CHECK(a.begin() == b.begin());
+      CHECK(a.end() == b.end());
+    }
+
+    SECTION("Move") {
+      auto a = WV(empty.begin(), empty.end());
+      auto begin = a.begin();
+      auto end = a.end();
+      auto b = std::move(a);
+      CHECK(b.begin() == begin);
+      CHECK(b.end() == end);
+    }
+  }
+
+  SECTION("Assign") {
+    SECTION("Copy") {
+      auto a = WV(empty.begin(), empty.end());
+      auto b = WV(one.begin(), one.end());
+      a = b;
+      CHECK(*(a.begin()) == "hi" );
+      CHECK(a.begin() == a.end() - 1);
+    }
+
+    SECTION("Move") {
+      auto a = WV(empty.begin(), empty.end());
+      auto b = WV(one.begin(), one.end());
+      a = std::move(b);
+      CHECK(*(a.begin()) == "hi" );
+      CHECK(a.begin() == a.end() - 1);
+    }
+  }
+
+  SECTION("at") {
+    auto v = WV(one.begin(), one.end());
+    CHECK(v.at(0) == "hi");
+    CHECK_THROWS_AS(v.at(1), std::out_of_range);
+  }
+
+  SECTION("operator[]") {
+    auto v = WV(two.begin(), two.end());
+    CHECK(v[0] == "one");
+    CHECK(v[1] == "two");
+  }
+
+  SECTION("front") {
+    auto v = WV(two.begin(), two.end());
+    CHECK(v.front() == "one");
+  }
+
+  SECTION("back") {
+    auto v = WV(two.begin(), two.end());
+    CHECK(v.back() == "two");
+  }
+
+  SECTION("begin") {
+    auto v = WV(two.begin(), two.end());
+    CHECK(*(v.begin()) == "one");
+    CHECK(*(v.begin() + 1) == "two");
+  }
+
+  SECTION("cbegin") {
+    auto v = WV(two.begin(), two.end());
+    CHECK(*(v.cbegin()) == "one");
+    CHECK(*(v.cbegin() + 1) == "two");
+  }
+
+  SECTION("end") {
+    auto v = WV(two.begin(), two.end());
+    CHECK(v.begin() + 2 == v.end());
+  }
+
+  SECTION("end") {
+    auto v = WV(two.begin(), two.end());
+    CHECK(v.cbegin() + 2 == v.cend());
+  }
+
+  SECTION("empty") {
+    CHECK(WV(one.begin(), one.begin()).empty());
+    CHECK(!WV(one.begin(), one.end()).empty());
+  }
+
+  SECTION("size") {
+    CHECK(WV(empty.begin(), empty.end()).size() == 0);
+    CHECK(WV(one.begin(), one.end()).size() == 1);
+    CHECK(WV(two.begin(), two.end()).size() == 2);
+  }
+
+  SECTION("swap") {
+    auto a = WV(one.begin(), one.end());
+    auto b = WV(two.begin(), two.end());
+
+    CHECK(a[0] == "hi");
+    CHECK(b[0] == "one");
+
+    std::swap(a, b);
+
+    CHECK(a[0] == "one");
+    CHECK(a.begin() + a.size() == a.end());
+    CHECK(b[0] == "hi");
+    CHECK(b.begin() + b.size() == b.end());
+  }
+
+  SECTION("operator==, operator!=") {
+    SECTION("self") {
+      auto a = WV(one.begin(), one.end());
+      CHECK(a == a);
+      CHECK(!(a != a));
+    }
+
+    SECTION("same iterators") {
+      auto a = WV(one.begin(), one.end());
+      auto b = WV(one.begin(), one.end());
+      CHECK(a == b);
+      CHECK(!(a != b));
+    }
+
+    SECTION("same values") {
+      auto one_copy = one;
+
+      auto a = WV(one.begin(), one.end());
+      auto b = WV(one_copy.begin(), one_copy.end());
+      CHECK(a == b);
+      CHECK(!(a != b));
+    }
+
+    SECTION("different values") {
+      auto one_copy = one;
+      one_copy.front() += "!!";
+
+      auto a = WV(one.begin(), one.end());
+      auto b = WV(one_copy.begin(), one_copy.end());
+      CHECK(!(a == b));
+      CHECK(a != b);
+    }
+
+    SECTION("different sizes") {
+      auto one_copy = one;
+      one_copy.push_back("lol");
+
+      auto a = WV(one.begin(), one.end());
+      auto b = WV(one_copy.begin(), one_copy.end());
+      CHECK(!(a == b));
+      CHECK(a != b);
+    }
+  }
+}
+
+}  // namespace shk
