c8f9ce76afbb394f3ff10d8dfe7f0f306790f419
Per Grön <per.eckerdal@gmail.com>
2017-04-25 15:46:10 +0200

    Add a stress test for shk-trace
    I was "hoping" to find that shk-trace sometimes misses inputs or outputs
    but this particular test was unable to prove that. It's still a useful
    script, especially if this will be ported to Linux/Windows in the future
    so I'm keeping it.

---
+++ src/benchmark/src/trace_stress_test.py
@@ -0,0 +1,138 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+
+# Program that stress tests shk-trace by tracing lots of commands in parallel
+# and verifying the trace output.
+
+import os
+import time
+import threading
+import pipes
+import re
+import shutil
+import subprocess
+import sys
+import sets
+
+def with_testdir():
+  tempdir = os.path.join(os.path.dirname(__file__), 'tmpdir')
+  def wrap(function):
+    def decorator(*args, **kwargs):
+      if os.path.exists(tempdir):
+        shutil.rmtree(tempdir)
+      os.mkdir(tempdir)
+      cwd = os.getcwd()
+      os.chdir(tempdir)
+      try:
+        result = function(*args, **kwargs)
+      finally:
+        os.chdir(cwd)
+        shutil.rmtree(tempdir)
+      return result
+    return decorator
+  return wrap
+
+def with_trace_server():
+  def wrap(function):
+    def decorator(*args, **kwargs):
+      trace_server = subprocess.Popen(
+          [shkTrace, '-s', '--suicide-when-orphaned'], stdout=subprocess.PIPE)
+      # Wait for server to actually start (would be better to parse output)
+      time.sleep(1)
+      try:
+        result = function(*args, **kwargs)
+      finally:
+        trace_server.terminate()
+      return result
+    return decorator
+  return wrap
+
+shkTraceFb = os.environ.get(
+    'SHK_TRACE_FB_PATH',
+    os.getcwd() + '/../../../build/src/shkutil/include/util')
+sys.path.insert(0, shkTraceFb)
+sys.path.insert(0, '../../../vendor/flatbuffers/python')
+import ShkTrace.Trace
+import ShkTrace.EventType
+
+shkTrace = os.environ.get(
+    'SHK_TRACE_PATH',
+    os.getcwd() + '/../../../build/bin/shk-trace')
+
+sys.path.insert(0, '/path/to/application/app/folder')
+
+def read_file(path):
+  with open(path) as f:
+    return f.read()
+
+def write_file(path, contents):
+  with open(path, 'w') as f:
+    f.write(contents)
+
+def run_cmd(cmd):
+  return subprocess.check_call(cmd, stderr=subprocess.STDOUT, shell=True)
+
+def parse_trace(data):
+  trace = ShkTrace.Trace.Trace.GetRootAsTrace(data, 0)
+  inputs = sets.Set()
+  for i in range(0, trace.InputsLength()):
+    input = trace.Inputs(i)
+    input = re.sub(r'T/hello\d?-......\.tbd', 'T/hello-XXXXXX.tbd', input)
+    inputs.add(input)
+
+  outputs = sets.Set()
+  for i in range(0, trace.OutputsLength()):
+    outputs.add(trace.Outputs(i))
+
+  error = ''
+  for i in range(0, trace.ErrorsLength()):
+    error += trace.Errors(i) + '\n'
+
+  if error:
+    raise Exception(error)
+
+  return [inputs, outputs]
+
+def trace_cmd(cmd, num):
+  file = "out." + str(num) + ".trace"
+  run_cmd(shkTrace + " -f " + file + " -c " + pipes.quote(cmd))
+  return parse_trace(bytearray(read_file(file)))
+
+class CompileManyThread (threading.Thread):
+  def __init__(self, num):
+    threading.Thread.__init__(self)
+    self.num = num
+
+  def run(self):
+    file = "hello" + str(self.num) + ".c"
+    write_file(
+        file,
+        "#include <stdio.h>\nint main(){printf(\"hello\\n\");return 0;}")
+    cmd = "clang " + file + " -o " + file + ".exe"
+    original = trace_cmd(cmd, self.num)
+    for i in range(1000):
+      print i
+      trace = trace_cmd(cmd, self.num)
+      if original != trace:
+        print "DIFFERENCE!"
+        print "Only in original inputs: " + str(original[0] - trace[0])
+        print "Only in now inputs: " + str(trace[0] - original[0])
+        print "Only in original outputs: " + str(original[1] - trace[1])
+        print "Only in now outputs: " + str(trace[1] - original[1])
+        break
+
+@with_testdir()
+@with_trace_server()
+def go():
+  threads = []
+
+  for i in range(8):
+    threads.append(CompileManyThread(i))
+
+  for t in threads:
+    t.start()
+
+  for t in threads:
+    t.join()
+
+go()
