7ebd08aa094339789920ef567b41ba09830e5dd3
Per Grön <per.eckerdal@gmail.com>
2017-05-29 08:29:14 +0200

    Add Iterate Rx source
    It is for making a stream from a C++ container.

---
+++ src/rx/include/rx/iterate.h
@@ -0,0 +1,74 @@
+// Copyright 2017 Per Grön. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <type_traits>
+
+#include <rx/subscription.h>
+
+namespace shk {
+
+/**
+ * Iterate takes a C++ container, for example an std::vector and returns a
+ * Publisher that emits the values of that container.
+ *
+ * Iterate makes a copy of the container every time the Publisher is subscribed
+ * to, in order to be able to give ownership of each value in the container to
+ * its Subscriber.
+ */
+template <typename Container>
+auto Iterate(Container &&container) {
+  return [container = std::forward<Container>(container)](
+      auto subscriber) {
+    class ContainerSubscription : public SubscriptionBase {
+     public:
+      ContainerSubscription(
+          const typename std::decay<Container>::type &container,
+          decltype(subscriber) &&subscriber)
+          : container_(container),
+            subscriber_(std::move(subscriber)),
+            it_(std::begin(container_)),
+            end_(std::end(container_)) {
+      if (it_ == end_) {
+        subscriber_.OnComplete();
+      }
+    }
+
+    void Cancel() {
+    }
+
+    void Request(size_t count) {
+      while (count-- && it_ != end_) {
+        subscriber_.OnNext(std::move(*it_++));
+        if (it_ == end_) {
+          subscriber_.OnComplete();
+        }
+      }
+    }
+
+     private:
+      typename std::decay<Container>::type container_;
+      decltype(subscriber) subscriber_;
+      decltype(std::begin(std::declval<Container>())) it_;
+      decltype(std::end(std::declval<Container>())) end_;
+    };
+
+    return ContainerSubscription(
+        container,
+        std::forward<decltype(subscriber)>(subscriber));
+  };
+}
+
+}  // namespace shk
---
+++ src/rx/test/iterate_test.cpp
@@ -0,0 +1,199 @@
+// Copyright 2017 Per Grön. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <catch.hpp>
+
+#include <vector>
+
+#include <rx/iterate.h>
+#include <rx/subscriber.h>
+
+namespace shk {
+
+TEST_CASE("Iterate") {
+  SECTION("construct") {
+    auto stream = Iterate(std::vector<int>{});
+  }
+
+  SECTION("empty container") {
+    auto stream = Iterate(std::vector<int>{});
+
+    int done = 0;
+    auto sub = stream(MakeSubscriber(
+        [](int next) { CHECK(!"should not happen"); },
+        [](std::exception_ptr &&error) { CHECK(!"should not happen"); },
+        [&done] { done++; }));
+    CHECK(done == 1);
+    sub.Request(1);
+    CHECK(done == 1);
+  }
+
+  SECTION("one value") {
+    auto stream = Iterate(std::vector<int>{ 1 });
+
+    int done = 0;
+    int next = 0;
+    auto sub = stream(MakeSubscriber(
+        [&next](int val) {
+          CHECK(val == 1);
+          next++;
+        },
+        [](std::exception_ptr &&error) { CHECK(!"should not happen"); },
+        [&done] {
+          done++;
+        }));
+    CHECK(done == 0);
+    CHECK(next == 0);
+    sub.Request(1);
+    CHECK(done == 1);
+    CHECK(next == 1);
+    sub.Request(1);
+    CHECK(done == 1);
+    CHECK(next == 1);
+  }
+
+  SECTION("multiple values, one at a time") {
+    auto stream = Iterate(std::vector<int>{ 1, 2 });
+
+    int done = 0;
+    int next = 0;
+    auto sub = stream(MakeSubscriber(
+        [&next](int val) {
+          if (next == 0) {
+            CHECK(val == 1);
+          } else if (next == 1) {
+            CHECK(val == 2);
+          } else {
+            CHECK(!"got too many values");
+          }
+          next++;
+        },
+        [](std::exception_ptr &&error) { CHECK(!"should not happen"); },
+        [&done, &next] {
+          CHECK(done == 0);
+          CHECK(next == 2);
+          done++;
+        }));
+    CHECK(done == 0);
+    CHECK(next == 0);
+
+    sub.Request(1);
+    CHECK(done == 0);
+    CHECK(next == 1);
+
+    sub.Request(1);
+    CHECK(done == 1);
+    CHECK(next == 2);
+
+    sub.Request(1);
+    CHECK(done == 1);
+    CHECK(next == 2);
+  }
+
+  SECTION("multiple values, all at once") {
+    auto stream = Iterate(std::vector<int>{ 1, 2 });
+
+    int done = 0;
+    int next = 0;
+    auto sub = stream(MakeSubscriber(
+        [&next](int val) {
+          if (next == 0) {
+            CHECK(val == 1);
+          } else if (next == 1) {
+            CHECK(val == 2);
+          } else {
+            CHECK(!"got too many values");
+          }
+          next++;
+        },
+        [](std::exception_ptr &&error) { CHECK(!"should not happen"); },
+        [&done, &next] {
+          CHECK(done == 0);
+          CHECK(next == 2);
+          done++;
+        }));
+    CHECK(done == 0);
+    CHECK(next == 0);
+
+    sub.Request(2);
+    CHECK(done == 1);
+    CHECK(next == 2);
+
+    sub.Request(1);
+    CHECK(done == 1);
+    CHECK(next == 2);
+  }
+
+  SECTION("multiple values, more than all at once") {
+    auto stream = Iterate(std::vector<int>{ 1, 2 });
+
+    int done = 0;
+    int next = 0;
+    auto sub = stream(MakeSubscriber(
+        [&next](int val) {
+          if (next == 0) {
+            CHECK(val == 1);
+          } else if (next == 1) {
+            CHECK(val == 2);
+          } else {
+            CHECK(!"got too many values");
+          }
+          next++;
+        },
+        [](std::exception_ptr &&error) { CHECK(!"should not happen"); },
+        [&done, &next] {
+          CHECK(done == 0);
+          CHECK(next == 2);
+          done++;
+        }));
+    CHECK(done == 0);
+    CHECK(next == 0);
+
+    sub.Request(Subscription::kAll);
+    CHECK(done == 1);
+    CHECK(next == 2);
+
+    sub.Request(1);
+    CHECK(done == 1);
+    CHECK(next == 2);
+  }
+
+  SECTION("multiple iterations") {
+    auto stream = Iterate(std::vector<int>{ 1 });
+
+    for (int i = 0; i < 3; i++) {
+      int done = 0;
+      int next = 0;
+      auto sub = stream(MakeSubscriber(
+          [&next](int val) {
+            CHECK(next == 0);
+            CHECK(val == 1);
+            next++;
+          },
+          [](std::exception_ptr &&error) { CHECK(!"should not happen"); },
+          [&done, &next] {
+            CHECK(done == 0);
+            CHECK(next == 1);
+            done++;
+          }));
+
+      CHECK(done == 0);
+      sub.Request(1);
+      CHECK(done == 1);
+      sub.Request(1);
+    }
+  }
+}
+
+}  // namespace shk
