64b85586d56a6b806b81581c4a47c83264f05e26
Per Grön <per.eckerdal@gmail.com>
2017-06-04 08:55:11 +0200

    Add Average Rx operator

---
+++ src/rs/include/rs/average.h
@@ -0,0 +1,42 @@
+// Copyright 2017 Per Grön. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <rs/map.h>
+#include <rs/pipe.h>
+#include <rs/reduce.h>
+
+namespace shk {
+
+template <typename ResultType = double>
+auto Average() {
+  using Accumulator = std::pair<ResultType, size_t>;
+  return Pipe(
+      Reduce(Accumulator(0, 0), [](Accumulator &&accum, auto &&value) {
+        accum.first += value;
+        accum.second++;
+        return accum;
+      }),
+      Map([](const Accumulator &accum) {
+        if (accum.second == 0) {
+          throw std::out_of_range(
+              "Cannot compute average value of empty stream");
+        } else {
+          return accum.first / accum.second;
+        }
+      }));
+}
+
+}  // namespace shk
---
+++ src/rs/test/average_test.cpp
@@ -0,0 +1,53 @@
+// Copyright 2017 Per Grön. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <catch.hpp>
+
+#include <vector>
+
+#include <rs/empty.h>
+#include <rs/average.h>
+#include <rs/iterate.h>
+
+#include "test_util.h"
+
+namespace shk {
+
+TEST_CASE("Average") {
+  auto average = Average();
+  static_assert(
+      IsPublisher<decltype(average(Empty()))>,
+      "Average stream should be a publisher");
+
+  SECTION("empty") {
+    auto error = GetError(average(Empty()));
+    CHECK(
+        GetErrorWhat(error) == "Cannot compute average value of empty stream");
+  }
+
+  SECTION("one value") {
+    CHECK(GetOne<double>(average(Iterate(std::vector<int>{ 10 }))) == 10);
+  }
+
+  SECTION("two values") {
+    CHECK(GetOne<double>(average(Iterate(std::vector<int>{ 10, 21 }))) == 15.5);
+  }
+
+  SECTION("int values") {
+    CHECK(GetOne<int>(
+        Average<int>()(Iterate(std::vector<int>{ 10, 21 }))) == 15);
+  }
+}
+
+}  // namespace shk
