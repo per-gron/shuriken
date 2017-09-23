3225f50de8870fd787b950b3e3ebd8f7bc974886
Per Grön <per.eckerdal@gmail.com>
2017-09-23 11:18:42 +0200

    Remove early not working reactive gRPC experiment code

--- src/shk-cache/src/shk-cache.cpp
+++ src/shk-cache/src/shk-cache.cpp
@@ -15,109 +15,14 @@
 #include <stdio.h>
 #include <thread>
 
-#include <grpc++/grpc++.h>
-
 #include <shk-cache/shkcache_generated.h>
 #include <shk-cache/shkcache.grpc.fb.h>
 #include <util/assert.h>
 
-#include "rx_grpc.h"
-#include "rx_grpc_flatbuffers.h"
-
 namespace shk {
 
-auto configGet(const Flatbuffer<ShkCache::ConfigGetRequest> &request) {
-  flatbuffers::FlatBufferBuilder response_builder;
-  auto store_config = ShkCache::CreateStoreConfig(
-      response_builder,
-      /*soft_store_entry_size_limit:*/1,
-      /*hard_store_entry_size_limit:*/2);
-  auto config_get_response = ShkCache::CreateConfigGetResponse(
-      response_builder,
-      store_config);
-
-  response_builder.Finish(config_get_response);
-
-  auto response = Flatbuffer<ShkCache::ConfigGetResponse>::fromBuilder(
-      &response_builder);
-
-  return rxcpp::observable<>::just(response);
-}
-
-RxGrpcServer makeServer() {
-  auto server_address = "0.0.0.0:50051";
-
-  RxGrpcServer::Builder builder;
-  builder.GrpcServerBuilder()
-      .AddListeningPort(server_address, grpc::InsecureServerCredentials());
-
-  builder.RegisterService<ShkCache::Config::AsyncService>()
-      .RegisterMethod<FlatbufferRefTransform>(
-          &ShkCache::Config::AsyncService::RequestGet,
-          &configGet);
-
-  return builder.BuildAndStart();
-}
-
-auto makeConfigGetRequest() {
-  flatbuffers::FlatBufferBuilder fbb;
-  auto config_get_request = ShkCache::CreateConfigGetRequest(fbb);
-  fbb.Finish(config_get_request);
-  return Flatbuffer<ShkCache::ConfigGetRequest>::fromBuilder(
-      &fbb);
-}
-
 int main(int /*argc*/, const char * /*argv*/[]) {
-  auto server = makeServer();
-  std::thread server_thread([&] { server.Run(); });
-
-  auto channel = grpc::CreateChannel(
-      "localhost:50051",
-      grpc::InsecureChannelCredentials());
-
-  RxGrpcClient client;  // TODO(peck): Rename to ClientFactory or Runloop
-
-  auto config_client = client.MakeClient<FlatbufferRefTransform>(
-      ShkCache::Config::NewStub(channel));
-
-  int requests_left = 1;
-  auto request_done = [&]() {
-    if (--requests_left == 0) {
-      client.Shutdown();
-    }
-  };
-  for (int i = 0; i < requests_left; i++) {
-    config_client
-        .Invoke(
-            &ShkCache::Config::Stub::AsyncGet, makeConfigGetRequest())
-        .subscribe(
-            [](Flatbuffer<ShkCache::ConfigGetResponse> response) {
-              if (auto config = response->config()) {
-                std::cout <<
-                    "RPC response: " <<
-                    config->soft_store_entry_size_limit() <<
-                    ", " <<
-                    config->hard_store_entry_size_limit() <<
-                    std::endl;
-              } else {
-                std::cout << "RPC response: [no config]" << std::endl;
-              }
-            },
-            [&request_done](std::exception_ptr error) {
-              // TODO(peck): Do something useful with this
-              std::cout << "OnError" << std::endl;
-              request_done();
-            },
-            [&request_done]() {
-              std::cout << "OnCompleted" << std::endl;
-              request_done();
-            });
-  }
-
-  client.Run();
-
-  server.Shutdown();
-  server_thread.join();
+  printf("Hello!\n");
 
   return 0;
 }
